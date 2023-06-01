95ee7fa1b6b91c1140db0cec2ccce1a275cd56ee
VALENTIN JULIAN LASORSA NIKCEVICH <46919212@ort>
2023-06-01 10:27:21 -0300

    formulario

--- src/components/formulario.js
+++ src/components/formulario.js
@@ -1,30 +1,30 @@
-import React from 'react';
-function Formulario(props){
-    return(
-        <div className="datos"></div>
-    )
-}
-function datos(props){
-    return (
-        <div className="datos">
-          <div className="datos-mascota">
-            {props.datos.mascota}
-          </div>
-          <div className="datos-propietario">
-            {props.datos.propietario}
-          </div>
-          <div className="datos-fecha">
-        {formatDate(props.fecha)}
-      </div>
-      <div className="datos-hora">
-        {formatTime(props.hora)}
-      </div>
-      <div className="datos-sintomas">
-        {props.datos.sintomas}
-      </div>
-        </div>
-      );
-}
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(element);
-export default Formulario
+import React, {useState} from 'react';
+export default function Formulario({form})
+const [mascota, setMascota] = useState("");
+const [propietario, setPropietario] = useState("");
+const [fecha, setFecha] = useState("");
+const [hora, setHora] = useState("");
+const [sintomas, setSintomas] = useState("");
+
+let datos={
+    nameMascota: input.value,
+    namePropietario: input.value,
+    fechaFecha: Date(),
+    horaHora: Time(),
+    sintomas: input.value
+};
+return(
+    <form onSubmit={agregarCita()}>
+    <label>Nombre Mascota</label>
+        <input onChange={this.handleChange} type="text" name="mascota" class="u-full-width" placeholder="Nombre Mascota" value=""></input>
+        <label>Nombre Dueño</label>
+        <input onChange={this.handleChange} type="text" name="propietario" class="u-full-width" placeholder="Nombre dueño de la mascota" value=""></input>
+        <label>Fecha</label>
+        <input onChange={this.handleChange} type="date" name="fecha" class="u-full-width" value=""></input>
+        <label>hora</label>
+        <input onChange={this.handleChange} type="time" name="hora" class="u-full-width" value=""></input>
+        <label>Sintomas</label>
+        <textarea name="sintomas" class="u-full-width"></textarea>
+        <button type="submit" class="u-full-width button-primary">Agregar Cita</button>
+      </form>
+      )
